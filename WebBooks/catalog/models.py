86dc2b634b331c6e18b91c1382e9e425a9873a22
Is-232 <piyanizinns@gmail.com>
2025-09-03 16:25:54 +0300

    Вывод фото в админке

--- WebBooks/catalog/models.py
+++ WebBooks/catalog/models.py
@@ -72,6 +72,9 @@ class Book(models.Model):
     photo = models.ImageField(upload_to='images',
                               help_text='Введите избражение обложки',
                               verbose_name='Изображение обложки')
+    def display_author(self):
+        return ', '.join([author.last_name for author in self.author.all ()])
+    display_author.short_descriptions = 'Авторы'
     def __str__(self):
         return self.title
     def get_absolute_url(self):
